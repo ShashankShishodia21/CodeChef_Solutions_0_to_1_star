f6699af01ae2dc7bd73ff7344c4505c2adb43c32
Shashank Shishodia <115582986+ShashankShishodia21@users.noreply.github.com>
2023-07-02 00:04:11 +0530

    Create FLOW010.c

---
+++ FLOW010.c
@@ -0,0 +1,11 @@
+t= int(input())
+for i in range(t):
+    c = str(input()).lower()
+    if c =='c':
+        print("Cruiser")
+    elif c =='b':
+        print("BattleShip")
+    elif c =='d':
+        print("Destroyer")
+    elif c =='f':
+        print("Frigate")
