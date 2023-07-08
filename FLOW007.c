18baa8e258f0609662111d70c52b281e301d41d9
Shashank Shishodia <115582986+ShashankShishodia21@users.noreply.github.com>
2023-07-08 23:49:56 +0530

    Update FLOW007.c

--- FLOW007.c
+++ FLOW007.c
@@ -8,12 +8,12 @@ int main() {
 	scanf("%d", &a);
 	while (a--) {
 
-		int n;
-		scanf("%d", &n);
+		int num;
+		scanf("%d", &num);
 		while(n!=0) {
-			rem = n % 10;
+			rem = num % 10;
 			sum = sum * 10 + rem;
-			n = n / 10;
+			num = num / 10;
 		}
 		printf("%d\n", sum);
 		sum = 0;
